e43fb447789a8696c859221c91985f18d693a65e
stoicinsight <65826912+StoicInsight@users.noreply.github.com>
2023-09-21 21:10:19 -0400

    finished excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+import 'bootstrap';
+import './style.css';
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import './assets/img/rigo-baby.jpg';
+import './assets/img/4geeks.ico';
+
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+
+let action = ['ate', 'peed', 'crushed', 'broke'];
+
+let what = ['my homework', 'the keys', 'the car'];
+
+let when = [
+  'before the class',
+  'right on time',
+  'when I finished',
+  'during my lunch',
+  'while I was praying',
+];
+
+const randomElement = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const excuse = document.querySelector('.excuse');
+
+console.log(randomElement(who));
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  excuse.innerHTML = `${randomElement(who)} ${randomElement(
+    action
+  )} ${randomElement(what)} ${randomElement(when)}`;
 };
